f738f1afa7d5f37aeca128fcf5fbb30f3e3baaf5
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-03-03 18:41:02 +0100

    namespaces update

--- ContactManager/ContactManager/obj/Debug/net9.0/ContactManager.AssemblyInfo.cs
+++ ContactManager/ContactManager/obj/Debug/net9.0/ContactManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6e996f1dcc02a1f09928c11196cc204a7d9823")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8913524332b8006c4c056556aaf76a6d0a7d8a88")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
