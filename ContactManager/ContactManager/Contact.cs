59c0358d06ea7de0055edc3868fbc50295ece473
JakubCzyzUE <61199748+JakubC0401@users.noreply.github.com>
2025-03-03 18:08:01 +0100

    .NET Update and create project

---
+++ ContactManager/ContactManager/Contact.cs
@@ -0,0 +1,20 @@
+﻿internal class Contact
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; } 
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public Contact(string firstName, string lastName, string email, string phoneNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public override string ToString()
+    {
+        return $"Imię: {FirstName}, Naziwsko: {LastName}, Email: {Email}, PhoneNumber: {PhoneNumber}";  
+    }
+}
